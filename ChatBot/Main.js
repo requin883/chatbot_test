43bf8259fe6ac46e492951b127e08ac1e9c9210b
montiellvazz <juanmontiel90ss@gmail.com>
2022-07-04 18:19:17 -0400

    feat: add invoice

--- ChatBot/Main.js
+++ ChatBot/Main.js
@@ -33,7 +33,6 @@ const bot = new TeleBot({
     bot.on('/zona',msg=>{
         let replyMarkup=bot.inlineKeyboard(boton(msg.data,bot))
         bot.sendMessage(msg.from.id, 'Trabajamos de 9:00 A.M - 6:00 P.M de lunes a viernes, y realizamos entregas en:', {replyMarkup});
-        //bot.sendMessage(msg.from.id,`${msg.from.first_name}, estamos trabajando:`,{replyMarkup},{replyMarkup});
     });
 
     bot.on('/view',msg=>{
@@ -45,11 +44,6 @@ const bot = new TeleBot({
         let replyMarkup=bot.inlineKeyboard(boton(msg.data,bot))
         createTrolley(msg.from.id);
         bot.sendMessage(msg.from.id,"Añadiendo al carrito",{replyMarkup});
-    });
-
-    bot.on('/fact',msg=>{
-        let replyMarkup=bot.inlineKeyboard(boton(msg.data,bot))
-        bot.sendMessage(msg.from.id,"Mostrando Factura",{replyMarkup});
     });
 
     bot.on('/search',msg=>{
@@ -72,5 +66,67 @@ const bot = new TeleBot({
         let replyMarkup = bot.inlineKeyboard(boton(msg.data, bot))
         bot.sendLocation(msg.from.id, [10.59882925256173, -71.65087523348457], {replyMarkup});
     });
+
+    //-------------------------------------- Generar factura
+
+    // bot.on('/fact',msg=>{
+    //     let replyMarkup = bot.inlineKeyboard(boton(msg.data,bot))
+    //     bot.sendMessage(msg.from.id,"Reviza tu factura para confirmar tu pago", {replyMarkup});
+    // });
+
+    bot.on('/fact', (msg) => {
+
+        const inlineKeyboard = bot.inlineKeyboard([
+            [
+                bot.inlineButton('Generar factura', {pay: true})
+            ]
+        ]);
+    
+        return bot.sendInvoice(msg.from.id, {
+            title: 'Factura de compra',
+            description: 'Node API Store',
+            payload: 'telebot-test-invoice',
+            providerToken: '284685063:TEST:MGZlNzhkZjkwOGZm',
+            startParameter: 'pay',
+            currency: 'USD',
+            prices: [
+                {label: 'Tea', amount: 125},
+                {label: 'For testing!', amount: 1250},
+            ],
+            need: {email: true, phoneNumbe: false},
+            // needShippingAdress: true,
+            // isFlexible: false,
+            // sendEmailToProvider: true,
+            replyMarkup: inlineKeyboard
+        }).then(data => {
+            console.log('OK', data);
+        }).catch(error => {
+            console.log('ERROR', error);
+        });
+    
+    });
+    
+    // bot.on('shippingQuery', (msg) => {
+    //     console.log('shippingQuery', msg);
+    // });
+    
+    // bot.on('preShippingQuery', (msg) => {
+    //     console.log('preShippingQuery', msg);
+    
+    //     const id = msg.id;
+    //     const isOk = true;
+    
+    //     return bot.answerPreCheckoutQuery(id, isOk);
+    
+    // });
+    
+    bot.on('successfulPayment', (msg) => {
+        console.log('successfulPayment', msg);
+    
+        return bot.sendMessage(msg.from.id, `Gracias por tu compra, ${msg.from.first_name}!`);
+    
+    });
+    
+    
     
 bot.start();
