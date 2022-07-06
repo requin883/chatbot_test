eab0bac7ac19c8eb51fc7a609ee8bb081fb11dbc
requin883 <requin883@gmail.com>
2022-07-06 06:23:43 -0700

    hotfix: add connection to DB to function

--- ChatBot/modules/show_trolley.js
+++ ChatBot/modules/show_trolley.js
@@ -6,15 +6,16 @@ async function getTrolley(msg,bot){
     let name = msg.from.first_name || msg.from.username;
     let replyMarkup = boton(msg.data,bot);
     try{
-        const res = await instance.get(`trolley?id=${10}`);
+        const res = await instance.get(`trolley?id=${id}`);
         const resData = res.data;
         if(!resData){
             replyMarkup = boton("/empty",bot);
             return bot.sendMessage(id, 
                 `${name}! El carrito todavía está vació por favor ingresa algún artículo para empezar a llenar tu carrito🔥`,{replyMarkup});
             }
-        let {Products} =resData[0];
-        bot.sendMessage(id,Products,{replyMarkup});
+        let {products} =resData[0];
+        products = products.join("\n");
+        bot.sendMessage(id,products,{replyMarkup});
     }catch(err){
     console.log(err);
     }
