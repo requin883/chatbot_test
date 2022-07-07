2faba379d84e674d901f5c47997c98cddee419f4
requin883 <requin883@gmail.com>
2022-07-06 18:19:59 -0700

    feat: add products message function and update show_trolley function to show message on client

--- ChatBot/modules/show_trolley.js
+++ ChatBot/modules/show_trolley.js
@@ -1,20 +1,22 @@
-require('module-alias/register');
 const instance = require("@utils");
 const boton = require("./buttons");
-
+const showProductsMessage = require("../utils/productsMessage")
 async function getTrolley(msg,bot){
     let id = msg.from.id;
     let name = msg.from.first_name || msg.from.username;
     let replyMarkup = boton(msg.data,bot);
     try{
         const res = await instance.get(`trolley?id=${id}`);
-        const products = res.data[0].data;
+        let products = res.data[0].data;
         if(products.length==0){
             replyMarkup = boton("/empty",bot);
             return bot.sendMessage(id,`${name}! El carrito todavía está vació por favor ingresa algún artículo para empezar a llenar tu carrito🔥`,{replyMarkup});
             }
-        products = products.join("\n");
-        bot.sendMessage(id,products,{replyMarkup});
+        else{
+            products = await showProductsMessage(products);
+            console.log(products);
+            bot.sendMessage(id,products,{replyMarkup});
+        }
     }catch(err){
     console.log(err);
     }
